74d467e5afdd7ef0a32c687367644a54d7279f46
mubassari <maulimubassari23@gmail.com>
2023-05-27 09:52:43 +0800

    update header

---
+++ dist/js/script.js
@@ -0,0 +1,187 @@
+$(document).ready(function () {
+  setTimeout(() => {
+    $("#preloader").slideUp(function () {
+      $(this).remove();
+    });
+  }, 1500);
+
+  $(".countdown").each((i, element) => {
+    const YEAR = $(element).data("year") || 0;
+    const MONTH = $(element).data("month") || 0;
+    const DAY = $(element).data("day") || 0;
+    const HOUR = $(element).data("hour") || 0;
+    const MINUTE = $(element).data("minute") || 0;
+    const SECOND = $(element).data("second") || 0;
+
+    $(element).countdown(`${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}:${SECOND}`, (event) => {
+      const plural = (time, text) => (event.strftime(time) == "1" || event.strftime(time) == "-1" ? text : text + "s");
+      $(element).html(event.strftime("%D " + plural("%D", "day") + " %H:%M:%S"));
+    });
+  });
+
+  $('[data-toggle="collapse"]').click(function () {
+    $(this).find("i.timeline-accordion-icon").toggleClass("fa-rotate-90");
+  });
+
+  const initCarousel = (carouselEl) => {
+    if (carouselEl.getAttribute("data-initialized") === "true") return; // Exit if already initialized
+    carouselEl.setAttribute("data-initialized", "true");
+
+    let cardWidth = $(".carousel-item", carouselEl).width();
+    let scrollPosition = 0;
+    let numOfCards = $(".carousel-item", carouselEl).length;
+    let numOfVisibleCards = Math.floor($(".carousel-inner", carouselEl).width() / cardWidth);
+    let numOfScrollableCards = numOfCards - numOfVisibleCards;
+
+    const SCROLL_SPEED = 1500;
+
+    $(".carousel-control-next", carouselEl).click(() => {
+      if (scrollPosition < cardWidth * numOfScrollableCards) {
+        scrollPosition += cardWidth;
+        $(".carousel-inner", carouselEl).animate({ scrollLeft: scrollPosition }, SCROLL_SPEED);
+      } else {
+        scrollPosition = 0;
+        $(".carousel-inner", carouselEl).animate({ scrollLeft: scrollPosition }, SCROLL_SPEED);
+      }
+    });
+
+    $(".carousel-control-prev", carouselEl).click(() => {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $(".carousel-inner", carouselEl).animate({ scrollLeft: scrollPosition }, SCROLL_SPEED);
+      } else {
+        scrollPosition = cardWidth * numOfScrollableCards;
+        $(".carousel-inner", carouselEl).animate({ scrollLeft: scrollPosition }, SCROLL_SPEED);
+      }
+    });
+
+    // Auto-scroll function
+    let autoScrollInterval;
+    const startAutoScroll = () =>
+      (autoScrollInterval = setInterval(() => $(".carousel-control-next", carouselEl).click(), 3000));
+    const stopAutoScroll = () => clearInterval(autoScrollInterval);
+
+    // Start auto-scrolling if window width >= 768px
+    const handleWindowSize = () => {
+      if (window.innerWidth >= 768) {
+        startAutoScroll();
+        $(carouselEl).removeClass("slide");
+      } else {
+        stopAutoScroll();
+        $(carouselEl).addClass("slide");
+      }
+    };
+
+    handleWindowSize();
+
+    $(window).on("resize", () => handleWindowSize());
+
+    // Stop autoscroll if hover
+    $(carouselEl).hover(
+      () => stopAutoScroll(),
+      () => startAutoScroll()
+    );
+
+    $("#mediaModal").hover(
+      () => stopAutoScroll(),
+      () => startAutoScroll()
+    );
+  };
+
+  $(".carousel:visible").each((i, el) => initCarousel(el));
+
+  $("[data-toggle='pill']").on("shown.bs.tab", (event) => {
+    $(event.target.getAttribute("data-target") + " .carousel").each((i, el) => initCarousel(el));
+  });
+
+  const modalCloseButton =
+    '<button type="button" class="modal-btn btn btn-dark rounded position-fixed" data-dismiss="modal"><i class="fa fa-times"></i></button>';
+
+  $('[data-toggle="modal"][data-target="#mediaModal"]').click(function () {
+    var mediaType = $(this).data("modal-type");
+
+    switch (mediaType) {
+      case "image":
+        var mediaSrc = $(this).data("media");
+        $("#mediaModal .modal-content").html(modalCloseButton + '<img class="rounded" src="' + mediaSrc + '">');
+        break;
+
+      case "image-list":
+        var mediaUrl = $(this).data("baseurl");
+        var mediaSrc = $(this).data("list");
+        var mediaImg = modalCloseButton;
+        mediaSrc.forEach((element) => {
+          mediaImg += '<img class="rounded" src="' + mediaUrl + element.trim() + '">';
+        });
+        $("#mediaModal .modal-content").html(mediaImg);
+
+        break;
+
+      case "video":
+        let videoPoster = $(this).prev("img").attr("src");
+        var mediaSrc = $(this).data("media");
+        $("#mediaModal .modal-content").html(
+          modalCloseButton +
+            '<div class="embed-responsive embed-responsive-16by9"><video class="embed-responsive-item" poster="' +
+            videoPoster +
+            '"controls> <source src="' +
+            mediaSrc +
+            '"></video></div>'
+        );
+        break;
+
+      case "info":
+        var modalContent = $(this).data("info-id");
+        var modalTitle = $(this).find(".title").text();
+        $("#mediaModal .modal-content").html(
+          '<div class="modal-header"><h5 class="modal-title font-weight-bold">' +
+            modalTitle +
+            '</h5><button type="button" class="close" data-dismiss="modal"><span>&times;</span></button></div><div class="modal-body">' +
+            $(modalContent).html() +
+            "</div></div>"
+        );
+
+        break;
+
+      case "embed":
+        var mediaSrc = $(this).data("media");
+        $("#mediaModal .modal-content").html(
+          modalCloseButton +
+            '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' +
+            mediaSrc +
+            '" allowfullscreen loading="lazy"></iframe></div>'
+        );
+        break;
+
+      default:
+        $("#mediaModal .modal-content").html("");
+        break;
+    }
+  });
+
+  $("#mediaModal").on("hidden.bs.modal", () => $("#mediaModal .modal-content").html(""));
+});
+
+const scrollToTop = () => {
+  const $navbar = $("#navbar");
+  const $bgImg = $(".bg-img");
+  const $scrollToTop = $(".scroll-to-top");
+
+  if ($(document).scrollTop() > 100) {
+    $navbar.removeClass("navbar-grow");
+    $bgImg.addClass("scrolled");
+    $scrollToTop.fadeIn(300);
+  } else {
+    $navbar.addClass("navbar-grow");
+    $bgImg.removeClass("scrolled");
+    $scrollToTop.fadeOut(300);
+  }
+};
+
+$(window).on("load", () => {
+  new WOW().init();
+
+  scrollToTop();
+});
+
+$(document).on("scroll", () => scrollToTop());
